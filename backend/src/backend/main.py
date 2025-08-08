ab0a20ca96a2dd347c757e190aa5b505184e5883
Taku <23535528+taku-shimomura@users.noreply.github.com>
2025-08-08 14:06:18 +0900

    /api/todos

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,7 +1,23 @@
+from typing import List, Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+
+class Todo(BaseModel):
+    task: str
+    done: bool = False
+
+
+todo_list: List[Optional[Todo]] = []
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get("/api/todos")
+async def todos() -> List[Optional[Todo]]:
+    return todo_list
+
+
+@app.post("/api/todos")
+async def create_todo(todo: Todo) -> List[Optional[Todo]]:
+    todo_list.append(todo)
+    return todo_list
