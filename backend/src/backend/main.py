36a1554bea90f9d8ae2792c01c3ef4045f67ee8d
Taku <23535528+taku-shimomura@users.noreply.github.com>
2025-08-08 14:33:55 +0900

    fetch cors

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 
@@ -10,6 +11,19 @@ class Todo(BaseModel):
 
 todo_list: List[Optional[Todo]] = []
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/todos")
